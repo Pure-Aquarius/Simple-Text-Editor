68c3059448a2ea803ce740472eb5ef843fbfa42d
Mohamed Afnan <aizen_affu@Mohameds-MacBook-Pro.local>
2025-09-11 14:05:59 +0100

    defined global variables: changed - to keep track if any changes to the text have been made, filename = holds name of current file, textbuf - a buffer to hold the text that is being edited

--- GUI.cpp
+++ GUI.cpp
@@ -1,7 +1,16 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
-#include <FL/Fl_Group.H>
 #include <FL/Fl_Button.H>
+#include <FL/Fl_Text_Editor.H>
+#include <FL/Fl_Text_Buffer.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Return_Button.H>
+#include <FL/Fl_Double_Window.H>
+using namespace std;
+
+int changed = 0;        //flag to indicate if text has been changed since last save
+char filename[256] = "";    //filename of current file
+Fl_Text_Buffer *textbuf;    //buffer to hold text being edited
 
 int main() {
     class Editor_Window : public Fl_Double_Window 
@@ -10,16 +19,20 @@ int main() {
             Editor_Window(int w, int h, const char *title);
             ~Editor_Window();
 
-            Fl_Window *replace_dlg;
-            Fl_Input *replace_find;
-            Fl_Input *replace_with;
-            Fl_Button *replace_all;
-            Fl_Return_Button *replace_next;
-            Fl_Button *replace_cancel;
+            Fl_Window *replace_dlg;     //actual winodow
+            Fl_Input *replace_find;     //text input that specifies what to search for
+            Fl_Input *replace_with;     //text input specifying what to replace with
+            Fl_Button *replace_all;     //button to replace all occurences
+            Fl_Return_Button *replace_next;     //button to replace next occurence
+            Fl_Button *replace_cancel;      //cancel replace operation
 
-            Fl_Text_Editor *text_editor;
+            Fl_Text_Editor *text_editor;        //main are for text editing
             char search [256];
-    }
-};
+    };
+
+    
+
+}
+
 
 
