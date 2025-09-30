4f252ba0ff8bc46d90f8b8f6d5692aa4c62484e1
Pure-Aquarius <infinityaffu@gmail.com>
2025-09-30 19:56:36 +0100

    moved the global variable here

--- editor.h
+++ editor.h
@@ -1,6 +1,41 @@
 #ifndef EDITOR_H
 #define EDITOR_H
 
+// --- REQUIRED HEADERS FOR DECLARATIONS ---
+// We include the headers needed to define our class and function prototypes.
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Double_Window.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Text_Editor.H>
+
+
+// --- CLASS DEFINITION ---
+// This is the "what" - it declares what our EditorWindow is.
+class EditorWindow: public Fl_Double_Window
+{
+    public:
+        EditorWindow(int w, int h, const char *title);
+
+        Fl_Window *replace_win = nullptr; // Pointer to the replace window
+        Fl_Input *find_input = nullptr;   // Pointer to the find input field
+        Fl_Input *replace_input = nullptr; // Pointer to the replace input field
+
+        Fl_Text_Editor *editor = nullptr; // Pointer to the text editor
+        char search_text[256] = {0}; // Text to find
+};
+
+
+// --- GLOBAL VARIABLES (EXTERN) ---
+// By using 'extern', we are telling any .cpp file that includes this header file
+// that these variables exist somewhere else (in our editor.cpp file).
+// This prevents "multiple definition" errors if you include this header in more than one file.
+                                    //NOTE: For a multi-window app, it's better to avoid globals.
+                                    // But for a single-window editor, this is acceptable.
+extern int changed_flag = 0;            //flag to indicate if changes have been made to a file
+extern char filename[256] = "";         //name of the current file
+extern Fl_Text_Buffer *textbuf = nullptr; //text buffer for the text editor
+
 //Function prototypes
 void changed_cb(int pos, int nInserted, int nDeleted, int nRestyled, const char *deletedText, void *v);
 void copy_cb(Fl_Widget*, void*);
@@ -26,19 +61,5 @@ void insert_cb(Fl_Widget*, void*);
 void view_cb(Fl_Widget*, void*);
 void close_cb(Fl_Widget*, void*);
 void undo_cb(Fl_Widget*, void*);
-
-//Window class
-class EditorWindow: public Fl_Double_Window
-{
-    public:
-        EditorWindow(int w, int h, const char *title);
-
-        Fl_Window *replace_win = nullptr; // Pointer to the replace window
-        Fl_Input *find_input = nullptr;   // Pointer to the find input field
-        Fl_Input *replace_input = nullptr; // Pointer to the replace input field
-
-        Fl_Text_Editor *editor = nullptr; // Pointer to the text editor
-        char search_text[256] = {0}; // Text to find
-};
 
 #endif
